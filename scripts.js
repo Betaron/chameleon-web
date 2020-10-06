c01a3f589854946a35e799df58854b1ec893d96e
Иван Агабалаев <agabalaev01@yandex.ru>
2020-10-06 22:49:38 +0300

    set up anchors and added pictures to the gallery

--- scripts.js
+++ scripts.js
@@ -1,13 +1,22 @@
 window.onload = () => {
-    setInterval(()=>{
-        if (document.querySelector('#switch1').checked){
+    setInterval(() => {
+        if (document.querySelector('#switch1').checked) {
             document.querySelector('#switch2').checked = true;
         }
-        else if(document.querySelector('#switch2').checked){
+        else if (document.querySelector('#switch2').checked) {
             document.querySelector('#switch3').checked = true;
         }
         else document.querySelector('#switch1').checked = true;
     }, 5000);
-    
+
 }
 
+window.onscroll = function() {
+    if(window.pageYOffset>150){
+      document.getElementById('arrow').style.display="block";
+  
+    }
+    else{
+       document.getElementById('arrow').style.display="none";
+    }
+  }
